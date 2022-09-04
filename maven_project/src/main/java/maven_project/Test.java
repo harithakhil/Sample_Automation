a96291f5c6b1dbd1dac47679678fcdf0d3c151ba
harithakhil <harithaharidas4134@gmail.com>
2022-09-04 22:22:47 +0530

    First Commit

---
+++ maven_project/src/main/java/maven_project/Test.java
@@ -0,0 +1,11 @@
+package maven_project;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hai Friends");
+		System.out.println("Welcome Obscura Zone");
+
+	}
+
+}
---
+++ maven_project/src/test/java/maven_project/HelloWorld.java
@@ -0,0 +1,13 @@
+package maven_project;
+
+import org.testng.annotations.Test;
+
+public class HelloWorld {
+	@Test
+	public void hellotest() {
+		System.out.println("Hello World");
+		System.out.println("Welcome Obscura Zone");
+		System.out.println("Hai All");
+	}
+	
+}
